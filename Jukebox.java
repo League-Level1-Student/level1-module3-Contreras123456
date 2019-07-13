9708915a1f842bed0d8d2020459348d21b63c300
Contreras123456 <35355212+Contreras123456@users.noreply.github.com>
2019-07-13 13:03:36 -0700

    Add files via upload

---
+++ Jukebox.java
@@ -0,0 +1,167 @@
+package _03_jukebox;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+/*   If you don't have javazoom.jar in your project, you can download it from here: http://bit.ly/javazoom
+ *   Right click your project and add it as a JAR (Under Java Build Path > Libraries).*/
+
+public class Jukebox implements Runnable, ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	Song song = new Song("bestsound.mp3");
+	Song song2 = new Song("bestsound2.mp3");
+	Song song3 = new Song("bestsound3.mp3");
+	public void run() {
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		button.setText("sound 1");
+		button2.setText("sound 2");
+		button3.setText("sound 3");
+		frame.pack();
+		// 1. Find an mp3 on your computer or on the Internet.
+		// 2. Create a Song object for that mp3
+		
+		// 3. Play the Song
+		song.play();
+		/*
+		 * 4. Create a user interface for your Jukebox so that the user can to choose
+		 * which song to play. You can use can use a different button for each song, or
+		 * a picture of the album cover. When the button or album cover is clicked, stop
+		 * the currently playing song, and play the one that was selected.
+		 */
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+	}
+
+	/* Use this method to add album covers to your Panel. */
+	private JLabel loadImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==button) {
+			song.play();
+		}
+		if (e.getSource()==button2) {
+			song2.play();
+		}
+		if (e.getSource()==button3) {
+			song3.play();
+		}
+	}
+
+}
+
+class Song {
+
+	private int duration;
+	private String songAddress;
+	private AdvancedPlayer mp3Player;
+	private InputStream songStream;
+
+	/**
+	 * Songs can be constructed from files on your computer or Internet addresses.
+	 * 
+	 * Examples: <code> 
+	 * 		new Song("everywhere.mp3"); 	//from default package 
+	 * 		new Song("/Users/joonspoon/music/Vampire Weekend - Modern Vampires of the City/03 Step.mp3"); 
+	 * 		new	Song("http://freedownloads.last.fm/download/569264057/Get%2BGot.mp3"); 
+	 * </code>
+	 */
+	public Song(String songAddress) {
+		this.songAddress = songAddress;
+	}
+
+	public void play() {
+		loadFile();
+		if (songStream != null) {
+			loadPlayer();
+			startSong();
+		} else
+			System.err.println("Unable to load file: " + songAddress);
+	}
+
+	public void setDuration(int seconds) {
+		this.duration = seconds * 100;
+	}
+
+	public void stop() {
+		if (mp3Player != null)
+			mp3Player.close();
+	}
+
+	private void startSong() {
+		Thread t = new Thread() {
+			public void run() {
+				try {
+					if (duration > 0)
+						mp3Player.play(duration);
+					else
+						mp3Player.play();
+				} catch (Exception e) {
+				}
+			}
+		};
+		t.start();
+	}
+
+	private void loadPlayer() {
+		try {
+			this.mp3Player = new AdvancedPlayer(songStream);
+		} catch (Exception e) {
+		}
+	}
+
+	private void loadFile() {
+		if (songAddress.contains("http"))
+			this.songStream = loadStreamFromInternet();
+		else
+			this.songStream = loadStreamFromComputer();
+	}
+
+	private InputStream loadStreamFromInternet() {
+		try {
+			return new URL(songAddress).openStream();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private InputStream loadStreamFromComputer() {
+		try {
+			return new FileInputStream(songAddress);
+		} catch (FileNotFoundException e) {
+			return this.getClass().getResourceAsStream(songAddress);
+		}
+	}
+}
---
+++ MagicBox.java
@@ -0,0 +1,119 @@
+package _04_magic_box;
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class MagicBox extends JPanel implements Runnable, MouseListener {
+	
+	/*
+	 * We are going to hide secrets within the magic box. 
+	 * When the user clicks on a secret place, stuff will happen.
+	 * 
+	 * 1. Make the frame respond to mouse clicks.
+	 *
+	 * 2. When the mouse is clicked, use the Media Palace (read the code in the magic_box package) to play sounds, 
+	 *    show images or speak.
+	 * 
+	 * 3. Choose 3 different locations on the background image.You can either use the mouse position, 
+	 *    or the color of the image, then decide what action the Media Palace should take in each case. 
+	 *     backgroundImage.getRGB(e.getX(), e.getY()) will give you the color of the current pixel.
+	 */
+
+	BufferedImage backgroundImage;
+
+
+	@Override
+	public void run() {
+		try {
+			loadBackgroundImage();
+			createUI();
+		} catch (Exception w) {
+			System.err.println(w.getMessage());
+		}
+	}
+
+	private void createUI() {
+		JFrame frame = new JFrame("The Magic Box contains many secrets...");
+		frame.add(this);
+		setPreferredSize(new Dimension(backgroundImage.getWidth(), backgroundImage.getHeight()));
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.addMouseListener(this);
+	}
+
+	private void loadBackgroundImage() throws Exception {
+		String imageFile = "src/_04_magic_box/magic-box.jpg";
+		try {
+			backgroundImage = ImageIO.read(new File(imageFile));
+		} catch (IOException e) {
+			throw new Exception("Could not load image: " + imageFile);
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(backgroundImage, 0, 0, null);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		MediaPalace mediapalace = new MediaPalace();
+		//mediapalace.playSoundFromInternet("https://www.youtube.com/watch?v=QIkDyi6Pon8");
+		int mouseColor = backgroundImage.getRGB(e.getX(), e.getY());
+		//System.out.println(backgroundImage.getRGB(e.getX(), e.getY()));
+		int blue = -13195858;
+		int gray = -10991808;
+		int orange = -2001142;
+		if (mouseColor==blue) {
+			mediapalace.speak("the fitnessgram pacer test is multistage aerobic capicity test");
+		}
+		if (mouseColor==gray) {
+			mediapalace.speak("minecraft");
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
